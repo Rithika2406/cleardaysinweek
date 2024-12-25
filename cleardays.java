d831074bb0b1bc00cd0fa798501c25dc7e6ae967
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-12-25 19:03:23 +0530

    Add files via upload

---
+++ cleardays.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner S=new Scanner(System.in);
+		int x=S.nextInt();
+		int y=S.nextInt();
+		int z=7-x-y;
+		System.out.println(z);
+
+	}
+}
